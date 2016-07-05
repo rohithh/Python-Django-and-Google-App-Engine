205a3fd20934db6fa969fefdbf1d281579fabbd2
rohithh <rohith_hy@yahoo.in>
2016-07-05 11:44:54 +0530

    GPS Tracker Web Application added

--- python-trial/main.py
+++ python-trial/main.py
@@ -1,14 +1,8 @@
-import os
-import webapp2
 import jinja2
+import webapp2
+import os
 
-from google.appengine.ext import db
-
-"""
-Program demonstrates user sign in using cookies
-"""
-
-template_dir = os.path.join(os.path.dirname(__file__), "templates")
+template_dir = os.path.join(os.path.dirname(__file__),"templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 class Handler(webapp2.RequestHandler):
@@ -26,20 +20,8 @@ class Handler(webapp2.RequestHandler):
 class MainHandler(Handler):
 
 	def get(self):
-		self.render("4_login.html")
-
-	def post(self):
-		username = str(self.request.get('username'))
-		self.response.headers.add_header('Set-Cookie', 'username = monkey; Path = /)')
-		self.redirect("/welcome")
-
-class WelcomeHandler(Handler):
-
-	def get(self):
-		username = self.request.cookies.get('username')
-		self.render("4_welcome.html", username = username)
+		self.render("map.html")
 
 app = webapp2.WSGIApplication([
-	('/signup', MainHandler),
-	('/welcome', WelcomeHandler)
+	('/', MainHandler)
 	])
